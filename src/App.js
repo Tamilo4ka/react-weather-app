9a44d1d34ce34b042f7cc6fd4389a43f5508c382
Tamilo4ka <sone4ko2019@gmail.com>
2022-09-01 15:35:39 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Tamila and is {""}
-        <a
-          href="https://github.com/Tamilo4ka/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Tamila and is {""}
+          <a
+            href="https://github.com/Tamilo4ka/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
